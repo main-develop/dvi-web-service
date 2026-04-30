fd9ec2725840d81b8b1b54237a13ce186831b7e8
main-develop <main.develop@protonmail.com>
2026-04-30 16:00:09 +0200

    fix: Mark the `width` prop as optional

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import logoImage from "@/public/logo.png";
 
-export const Logo = ({ width = 55 }: { width: number }) => {
+export const Logo = ({ width = 55 }: { width?: number }) => {
   return (
     <Link href="/">
       <Image src={logoImage} alt="logo" width={width} className="relative select-none" />
